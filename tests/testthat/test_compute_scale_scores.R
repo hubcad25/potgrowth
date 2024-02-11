ea32174579e55d052916560e45832bcf53c3ebf2
hubcad25 <hubertcadieux@gmail.com>
2024-02-11 17:27:01 -0500

    fix tests

--- tests/testthat/test_compute_scale_scores.R
+++ tests/testthat/test_compute_scale_scores.R
@@ -2,14 +2,16 @@ library(testthat)
 library(potgrowth)
 
 # Créer des données factices pour les tests
-factanal_object <- list(loadings = matrix(c(0.5, 0.2, -0.7, 2, -4, 1), nrow = 3, byrow = TRUE,
+factanal_object <- list(loadings = matrix(c(0.5, 0.2, -0.7, 2, -4, 1), nrow = 3, byrow = FALSE,
                                           dimnames = list(c("A", "B", "C"), NULL)))
 survey_data <- data.frame(A = c(1, 2, 3), B = c(3, 0, 1), C = c(2, 3, 1))
 scale_order <- 1
 
 # Test 1: Vérifier que la fonction retourne des valeurs correctes pour des données d'entrée valides
 test_that("compute_scale_scores returns correct values", {
-  expected_scores <- c(1 * 0.5 + 3 * 0.2 + 2 * -0.7, 2 * 0.5 + 0 * 0.2 + 3 * -0.7, 3 * 0.5 + 1 * 0.2 + 1 * -0.7)
+  expected_scores <- c(0.5 * 1 + 0.2 * 3 - 0.7 * 2,
+                       0.5 * 2 + 0.2 * 0 - 0.7 * 3,
+                       0.5 * 3 + 0.2 * 1 - 0.7 * 1)
   computed_scores <- compute_scale_scores(factanal_object, survey_data, scale_order)
   expect_equal(computed_scores, expected_scores)
 })
