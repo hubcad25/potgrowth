b852f5789ded865515c8d96b8cffd69dcc13df46
hubcad25 <hubertcadieux@gmail.com>
2024-11-22 20:09:39 -0500

    add link

--- R/bloc_recursive_frequentist.R
+++ R/bloc_recursive_frequentist.R
@@ -1,19 +1,20 @@
 #' Fits a Gamma regression model to data and plots the estimated and real density
 #'
 #' @param formula A formula specifying the response and predictor variables.
+#' @param link The link function to use for the model. By default, the "identity" link is used.
 #' @param data A dataframe containing the variables in the formula.
 #' @param plot A logical. If TRUE, the function plots the estimated and real density. If FALSE, no plot is produced.
 #' @importFrom stats glm
 #' @importFrom ggplot2 ggplot geom_histogram geom_density stat_function scale_fill_manual scale_color_manual labs
 #' @export
-gamma_model <- function(formula, data, plot = TRUE) {
+gamma_model <- function(formula, link = "identity", data, plot = TRUE) {
   # Transform the response variable
   response_var <- all.vars(formula)[1]
   data[[response_var]] <- (data[[response_var]] + 1) / 2
   data[[response_var]] <- ifelse(data[[response_var]] == 0, 1e-6, data[[response_var]])
   data$response_var <- data[[response_var]]
   # Fit the gamma regression model
-  model <- stats::glm(formula, data = data, family = Gamma(link = "identity"))
+  model <- stats::glm(formula, data = data, family = Gamma(link = link))
   
   # Calculate shape and scale
   shape <- summary(model)$dispersion
